b52422aacd65033e5b6f82705d6016defa80ed97
nate <n4t4nistorus@gmail.com>
2022-04-21 20:19:08 +0200

    test

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: nbenhado <nbenhado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 21:48:38 by nbenhado          #+#    #+#             */
-/*   Updated: 2022/04/21 20:16:22 by nbenhado         ###   ########.fr       */
+/*   Updated: 2022/04/21 20:19:01 by nbenhado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	main(int ac, char **av)
 {
 	long long	start_of_program;
 	t_data		*data;
-
+	//test
 	start_of_program = get_actual_time();
 	if (!error_handling(ac, av))
 		return (-1);
