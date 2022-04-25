890bee2e504b379fd04993d31258751e4eab0c32
Nathan BENHADOUCHE <nbenhado@e3r8p19.clusters.42paris.fr>
2022-04-25 18:48:40 +0200

    program too slow for 200 philos

--- sources/actions.c
+++ sources/actions.c
@@ -6,7 +6,7 @@
 /*   By: nbenhado <nbenhado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 21:50:30 by nbenhado          #+#    #+#             */
-/*   Updated: 2022/04/25 15:14:53 by nbenhado         ###   ########.fr       */
+/*   Updated: 2022/04/25 18:40:11 by nbenhado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,30 +39,33 @@ int	stop_conditions(t_philo *philo)
 	return (0);
 }
 
+// if (philo->id != philo->data->nbof_philos)
+	// pthread_mutex_lock(&philo->data->forks[philo->r_fork]);
+// else
+	// pthread_mutex_lock(&philo->data->forks[philo->l_fork]);
+// print_msg("has taken a fork", philo);
+// if (philo->id != philo->data->nbof_philos)
+	// pthread_mutex_lock(&philo->data->forks[philo->l_fork]);
+// else
+	// pthread_mutex_lock(&philo->data->forks[philo->r_fork]);
 int	grab_forks_n_eat(t_philo *philo)
 {
 	if (not_dead(philo->data))
 	{
-		if (philo->id != philo->data->nbof_philos)
-			pthread_mutex_lock(&philo->data->forks[philo->r_fork]);
-		else
-			pthread_mutex_lock(&philo->data->forks[philo->l_fork]);
+		pthread_mutex_lock(&philo->data->forks[philo->r_fork]);
 		print_msg("has taken a fork", philo);
-		if (philo->id != philo->data->nbof_philos)
-			pthread_mutex_lock(&philo->data->forks[philo->l_fork]);
-		else
-			pthread_mutex_lock(&philo->data->forks[philo->r_fork]);
+		pthread_mutex_lock(&philo->data->forks[philo->l_fork]);
 		print_msg("has taken a fork", philo);
-		if (!stop_conditions(philo))
-		{
+		// if (!stop_conditions(philo))
+		// {
 			print_msg("is eating", philo);
 			if (!check_max_eat(philo))
 				return (0);
 			philo->last_eat = current_time();
 			usleep(philo->data->time_to_eat * 1000);
-		}
-		pthread_mutex_unlock(&philo->data->forks[philo->l_fork]);
+		// }
 		pthread_mutex_unlock(&philo->data->forks[philo->r_fork]);
+		pthread_mutex_unlock(&philo->data->forks[philo->l_fork]);
 	}
 	return (1);
 }
@@ -91,8 +94,8 @@ void	*threads_act(void *arg)
 			if (current_time() >= philo->last_eat + philo->data->time_to_die)
 				return (print_msg("died", philo), NULL);
 	}
-	if (philo->id % 2 != 0)
-		usleep(19000);
+	if (philo->id % 2 == 0)
+		usleep(20000);
 	while (!stop_conditions(philo))
 	{
 		if (grab_forks_n_eat(philo))
